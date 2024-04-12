e1d2ecc0d527d0fa6f7658357b53598f1342f11c
Louis <u1152222@umail.usq.edu.au>
2024-04-12 12:57:15 +1000

    Add hover effect, add two event listeners: mouseover, mouseend

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ const body = document.querySelector(".container");
 const gridsquare = document.createElement("div");
 gridsquare.className = "grid";
 body.appendChild(gridsquare);
-for (let i = 1;i<16;i++){
+for (let i = 1;i<256;i++){
     body.appendChild(gridsquare.cloneNode(true));
 }
 
 const l = document.querySelectorAll(".grid");
+l.forEach((l) => {
+    l.addEventListener("mouseover",function (e) {
+        e.target.style.background = "blue";
+    })
+})
 
+l.forEach((l) => {
+    l.addEventListener("mouseleave",function (e) {
+        e.target.style.background = "black";
+    })
+})
     
 
 
