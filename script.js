4ce8e741dbc8498a9998cdefaa9bd2942ecea5e5
Louis <u1152222@umail.usq.edu.au>
2024-04-13 15:26:25 +1000

    Add button prompts user input sizegrid, deletes grid + appends user size to grid

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const body = document.querySelector(".container");
 const gridsquare = document.createElement("div");
 gridsquare.className = "grid";
 body.appendChild(gridsquare);
+
 for (let i = 1;i<256;i++){
     body.appendChild(gridsquare.cloneNode(true));
 }
@@ -14,11 +15,25 @@ l.forEach((l) => {
     })
 })
 
-/* l.forEach((l) => {
-    l.addEventListener("mouseleave",function (e) {
-        e.target.style.background = "black";
-    })
-}) */
+const button = document.querySelector("button");
+let numSquare;
+button.addEventListener("click", () => {
+    numSquare = parseInt(prompt("Enter number of squares per side for new grid"));
+    if(numSquare>100){
+        alert("Please enter value less than 100");
+        return;
+    }
+    while(body.firstChild){
+        body.removeChild(body.firstChild);
+    }
+
+    for(let i = 0;i<numSquare;i++){
+        body.appendChild(gridsquare.cloneNode(true));
+    }
+    console.log(numSquare);
+
+})
+
     
 
 
