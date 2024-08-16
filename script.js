e31d33d1a002ae3342ec82ae46ef53ba375f7c2e
Louis <u1152222@umail.usq.edu.au>
2024-08-16 16:47:24 +1000

    Restart javascript add createGrid function

--- script.js
+++ script.js
@@ -1,47 +1,13 @@
-const body = document.querySelector(".container");
+const container = document.querySelector(".container")
+const grid = document.createElement("div")
+grid.classList.add("grid")
 
-const gridsquare = document.createElement("div");
-gridsquare.className = "grid";
-body.appendChild(gridsquare);
-
-for (let i = 1;i<256;i++){
-    body.appendChild(gridsquare.cloneNode(true));
+function createGrid(num){
+    for (let i = 0;i<num;i++){
+        const clone = grid.cloneNode(true);
+        container.appendChild(clone)
+    }
+    
 }
 
-const l = document.querySelectorAll(".grid");
-l.forEach((l) => {
-    l.addEventListener("mouseover",function (e) {
-        e.target.style.background = "blue";
-    })
-})
-
-const button = document.querySelector("button");
-let numSquare;
-button.addEventListener("click", () => {
-    numSquare = parseInt(prompt("Enter number of squares per side for new grid"));
-    if(numSquare>100){
-        alert("Please enter value less than 100");
-        return;
-    }
-    while(body.firstChild){
-        body.removeChild(body.firstChild);
-    }
-
-    for(let i = 0;i<numSquare;i++){
-        body.appendChild(gridsquare.cloneNode(true));
-    }
-
-    const l = document.querySelectorAll(".grid");
-    l.forEach((l) => {
-    l.addEventListener("mouseover",function (e) {
-        e.target.style.background = "blue";
-    })
-})
-    console.log(numSquare);
-
-})
-
-    
-
-
-
+createGrid(10)
